ea5fb88dcd91746061bbcd38c54f907781118f6d
supuni97 <sitharabandara1997@gmail.com>
2025-09-07 19:07:06 +0530

    Initial commit

---
+++ src/main/java/com/example/email/service/EmailService.java
@@ -0,0 +1,52 @@
+package com.example.email.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import java.util.Locale;
+
+@Service
+public class EmailService {
+
+    private static TemplateEngine templateEngine;
+    private static Context context;
+
+    private JavaMailSender emailSender;
+
+    private Logger  LOG = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    public void setEmailSender(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    private void sendEmail(String mailTo, String subject, String mailBody) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper  helper  = new MimeMessageHelper(message, true);
+        helper.setTo(mailTo);
+        helper.setSubject(subject);
+        helper.setText(mailBody, true);
+    }
+
+    private static void initializeTemplateEngine() {
+
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode("HTML5");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+        context = new Context(Locale.US);
+
+    }
+
+}
